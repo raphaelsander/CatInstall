7462a3e9af6939ba30cbd2f0e920918f898733f8
Raphael Sander <raphael.sander75@gmail.com>
2016-07-20 20:19:56 -0300

    Nova estrutura.

--- AppInstall/main.py
+++ AppInstall/main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # coding=UTF-8
 
-## By Raphael Sander
-## <raphael.sander75@gmail.com>
+# By Raphael Sander
+# <raphael.sander75@gmail.com>
 
 from Tkinter import *
 from PIL import ImageTk, Image
--- dcc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#!/bin/bash
-DIR='/opt/AppInstall/.cache/'
-mkdir $DIR
