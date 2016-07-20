b7a9aed04ce210d4a0e94148ebaad3943b93c645
Raphael Sander <raphael.sander75@gmail.com>
2016-07-20 20:19:56 -0300

    Alterações na criação da pasta de cache. Criação da janela About sem nenhum componente.

--- AppInstall/main.py
+++ AppInstall/main.py
@@ -9,12 +9,19 @@ from PIL import ImageTk, Image
 import os
 import os.path
 
-os.system("mkdir ~/.cacheappinstall/")
+user = os.path.expanduser('~/')
 
+if os.path.exists('%s/.cacheappinstall/' % user) == True:
+    pass
+else:
+    os.system("mkdir ~/.cacheappinstall/")
+
+global p
+p = "/home/raphael/Projetos/AppInstall/AppInstall"
 
 class App:
     def __init__(self, master):
-
+        self.aboutw = None
         self.img = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
         self.var = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
@@ -24,10 +31,10 @@ class App:
             self.var[x] = IntVar()
 
         Button(relief=FLAT, command=self.bash, text="Instalar").grid(column=1, row=6)
-        Button(relief=FLAT, command=self.bash, text="About").grid(column=3, row=6)
+        Button(relief=FLAT, command=self.about, text="About").grid(column=3, row=6)
 
         for x in range(0, 12):
-            self.img[x] = ImageTk.PhotoImage(Image.open(u"/opt/AppInstall/images/noinstalled/%s.png" % x))
+            self.img[x] = ImageTk.PhotoImage(Image.open(u"%s/images/noinstalled/%s.png" % (p, x)))
             if x < 6:
                 Label(image=self.img[x]).grid(column=1, row=x)
                 Checkbutton(master, variable=self.var[x]).grid(column=0, row=x)
@@ -38,19 +45,33 @@ class App:
     def bash(self):
         for x in range(0, 12):
             if self.var[x].get() == 1:
-                os.system("gnome-terminal -x bash -c /opt/AppInstall/apps/%s && exit; exec bash" % x)
+                os.system("gnome-terminal -x bash -c %s/apps/%s && exit; exec bash" % (p, x))
 
+    def about(self):
+        if self.aboutw is None:
+            self.aboutw = Tk()
+            self.aboutw.title('Sobre')
+            self.aboutw.geometry('500x360+100+250')
+            self.aboutw.maxsize(500, 360)
+            self.aboutw.protocol("WM_DELETE_WINDOW", self.close_about)
+            self.icon = ImageTk.PhotoImage(Image.open(u"%s/icon.png" % p))
+
+        else:
+            self.aboutw.lift()
+
+    def close_about(self):
+        self.aboutw.destroy()
+        self.aboutw = None
 
 def main():
     root = Tk()
-    app = App(root)
+    App(root)
     root.title('AppInstall')
     root.geometry('500x360+100+250')
     root.maxsize(500, 360)
-    icon = ImageTk.PhotoImage(Image.open(u"/opt/AppInstall/icon.png"))
+    icon = ImageTk.PhotoImage(Image.open(u"%s/icon.png" % p))
     root.tk.call('wm', 'iconphoto', root._w, icon)
     root.mainloop()
-
 
 if __name__ == '__main__':
     main()
