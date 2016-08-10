29ff8b96c621d1ba67f6c3e468825a109b38004f
Cainã D'Ajuda <cainan13@gmail.com>
2016-08-10 20:02:24 -0300

    Simplifying call scripts

--- main.py
+++ main.py
@@ -19,6 +19,21 @@ class _UiProxy(object):
 
 class CatApp(object):
 
+    # This dictionary is a referente to script app using button label
+    applications = {
+        'Android Studio' : 'android_studio',
+        'Chrome' : 'chrome',
+        'Numix' : 'numix',
+        'Popcorn Time' : 'popcorn',
+        'SS Recorder' : 'simplescreenrecorder',
+        'Skype' : 'skype',
+        'Spotify' : 'spotify',
+        'Steam' : 'steam',
+        'Subterfuge' : 'subterfuge',
+        'Telegram' : 'telegram',
+        'Ubuntu Tweak' : 'ubuntu_tweak',
+    }
+
     def __init__(self):
         self._ui = _UiProxy()
         self._setup()
@@ -41,18 +56,17 @@ class CatApp(object):
         main_window = self._ui.main_window
         main_window.connect("destroy", Gtk.main_quit)
 
-        self._ui.chrome_btn.connect('clicked', self._on_chrome_btn__clicked)
-        self._ui.everpad_btn.connect('clicked', self._on_everpad_btn__clicked)
-        self._ui.numix_btn.connect('clicked', self._on_numix_btn__clicked)
-        self._ui.popcorn_btn.connect('clicked', self._on_popcorn_btn__clicked)
-        self._ui.recorder_btn.connect('clicked', self._on_recorder_btn__clicked)
-        self._ui.skype_btn.connect('clicked', self._on_skype_btn__clicked)
-        self._ui.spotify_btn.connect('clicked', self._on_spotify_btn__clicked)
-        self._ui.steam_btn.connect('clicked', self._on_steam_btn__clicked)
-        self._ui.studio_btn.connect('clicked', self._on_studio_btn__clicked)
-        self._ui.subterfuge_btn.connect('clicked', self._on_subterfuge_btn__clicked)
-        self._ui.telegram_btn.connect('clicked', self._on_telegram_btn__clicked)
-        self._ui.ubuntu_tweak_btn.connect('clicked', self._on_ubuntu_tweak_btn__clicked)
+        self._ui.chrome_btn.connect('clicked', self._on_app_btn__clicked)
+        self._ui.numix_btn.connect('clicked', self._on_app_btn__clicked)
+        self._ui.popcorn_btn.connect('clicked', self._on_app_btn__clicked)
+        self._ui.recorder_btn.connect('clicked', self._on_app_btn__clicked)
+        self._ui.skype_btn.connect('clicked', self._on_app_btn__clicked)
+        self._ui.spotify_btn.connect('clicked', self._on_app_btn__clicked)
+        self._ui.steam_btn.connect('clicked', self._on_app_btn__clicked)
+        self._ui.studio_btn.connect('clicked', self._on_app_btn__clicked)
+        self._ui.subterfuge_btn.connect('clicked', self._on_app_btn__clicked)
+        self._ui.telegram_btn.connect('clicked', self._on_app_btn__clicked)
+        self._ui.ubuntu_tweak_btn.connect('clicked', self._on_app_btn__clicked)
 
         main_window.show_all()
 
@@ -67,59 +81,12 @@ class CatApp(object):
     #   Callbacks
     #
 
-    # Aqui deve-se colocar os callbacks de todos os botoes que irao ocorrer a
-    # instalacao
-
     def _on_about_btn__clicked(self, button):
         print "about"
 
-    def _on_chrome_btn__clicked(self, button):
-        print "chrome"
-        self._install_app('chrome')
-
-    def _on_everpad_btn__clicked(self, button):
-        print "everpad"
-        self._install_app('everpad')
-
-    def _on_numix_btn__clicked(self, button):
-        print "numix"
-        self._install_app('numix')
-
-    def _on_popcorn_btn__clicked(self, button):
-        print "popcorn"
-        self._install_app('popcorn')
-
-    def _on_recorder_btn__clicked(self, button):
-        print "recorder"
-        self._install_app('recorder')
-
-    def _on_skype_btn__clicked(self, button):
-        print "skype"
-        self._install_app('skype')
-
-    def _on_spotify_btn__clicked(self, button):
-        print "spotify"
-        self._install_app('spotify')
-
-    def _on_steam_btn__clicked(self, button):
-        print "steam"
-        self._install_app('steam')
-
-    def _on_studio_btn__clicked(self, button):
-        print "studio"
-        self._install_app('android_studio')
-
-    def _on_subterfuge_btn__clicked(self, button):
-        print "subterfuge"
-        self._install_app('subterfuge')
-
-    def _on_telegram_btn__clicked(self, button):
-        print "telegram"
-        self._install_app('telegram')
-
-    def _on_ubuntu_tweak_btn__clicked(self, button):
-        print "tweak"
-        self._install_app('ubuntu_tweak')
+    def _on_app_btn__clicked(self, button):
+        app_name = self.applications.get(button.get_label())
+        self._install_app(app_name)
 
 
 if __name__=='__main__':
