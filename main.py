335bc357cd144c81c79ca2f124e4adcef253884c
Raphael Sander <raphael.sander75@gmail.com>
2016-09-08 23:30:16 -0300

    Tentando setar icon para cada botão.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # By Team BlackCat
 
 import os
-import os.path
+#import os.path
 import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
@@ -33,15 +33,23 @@ class App(Gtk.Window):
 		print ("Fechando")
 		Gtk.main_quit()
 
-	def aplicativos(self, data=None):
+	def aplicativos(self, apps, *args):
 		apps = os.listdir(os.getcwd() + "/apps/")
 		print ("Exibindo janela de Aplicativos")
 		self.window.hide()
 		self.window = self.builder.get_object("window2")
+
+		#img = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 		#for i in range(0, len(apps)):
 		for i in range(0, 9):
-			print (i, apps[i])
+			img = Gtk.Image()
+			img.set_from_file(os.getcwd() + "/ui/apps/%s.svg" %apps[i])
+			#print(i, apps[i], img[i])
+			#app = ("%s" % apps[i])
 			self.builder.get_object("%s" %i).set_label("%s" %apps[i])
+			self.builder.get_object("%s" %i).connect('clicked', self.install_app, apps[i])
+			self.builder.get_object("%s" %i).set_image(img)
 		self.window.show_all()
 
 	def home(self, data=None):
@@ -50,9 +58,11 @@ class App(Gtk.Window):
 		self.window = self.builder.get_object("window1")
 		self.window.show()
 
-	#def install_app(self, app):
-		#scripts_path = os.getcwd() + "/apps/"
-		#os.system("gnome-terminal -x bash -c %s%s && exit; exec bash" % (scripts_path, app))
+	def install_app(self, apps, *args):
+		scripts_path = os.getcwd() + "/apps/"
+		print (scripts_path)
+		os.system("gnome-terminal -x bash -c %s%s && exit; exec bash" %(scripts_path, apps))
+		print (scripts_path, app)
 
 	#def setup(self):
 		#for i in self.applications:
