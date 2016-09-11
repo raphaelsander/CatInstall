f1f078edef0bd760129feef509dcf19fc5e75b58
Raphael Sander <raphael.sander75@gmail.com>
2016-09-11 17:22:20 -0300

    Chamando bash automaticamente. Erro de duplicidade de connect corrigido.

--- main.py
+++ main.py
@@ -4,7 +4,6 @@
 # By Team BlackCat
 
 import os
-#import os.path
 import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
@@ -20,9 +19,9 @@ class App(Gtk.Window):
 		self.builder = Gtk.Builder()
 		self.builder.add_from_file(os.getcwd() + "/ui/main.glade")
 		self.builder.connect_signals(self)
-		self.window = self.builder.get_object("window1")
+		self.window1 = self.builder.get_object("window1")
 		self.aboutdialog = self.builder.get_object("aboutdialog1")
-		self.window.show()
+		self.window1.show()
 		
 	def about(self, data=None):
 		print ("Exibindo Sobre")
@@ -33,41 +32,36 @@ class App(Gtk.Window):
 		print ("Fechando")
 		Gtk.main_quit()
 
-	def aplicativos(self, apps, *args):
+	def aplicativos(self, data=None):
 		apps = os.listdir(os.getcwd() + "/apps/")
 		print ("Exibindo janela de Aplicativos")
-		self.window.hide()
-		self.window = self.builder.get_object("window2")
+		self.window1.hide()
 
-		#img = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+		try:
+			self.window2.show_all()
+		except:
+			self.window2 = self.builder.get_object("window2")
+			for i in range(0, 9):
+				self.builder.get_object("%s" %i).set_label("%s" %apps[i])
+				self.builder.get_object("%s" %i).connect('clicked', self.install_app, "%s" %apps[i])
+			self.window2.show_all()
 
-		#for i in range(0, len(apps)):
-		for i in range(0, 9):
-			img = Gtk.Image()
-			img.set_from_file(os.getcwd() + "/ui/apps/%s.svg" %apps[i])
-			#print(i, apps[i], img[i])
-			#app = ("%s" % apps[i])
-			self.builder.get_object("%s" %i).set_label("%s" %apps[i])
-			self.builder.get_object("%s" %i).connect('clicked', self.install_app, apps[i])
-			self.builder.get_object("%s" %i).set_image(img)
-		self.window.show_all()
+	def next(self):
+		print ("Próxima página")
+
+	def back(self):
+		print("Página anterior")
 
 	def home(self, data=None):
 		print ("Voltando ao Início")
-		self.window.hide()
-		self.window = self.builder.get_object("window1")
-		self.window.show()
+		self.window2.hide()
+		self.window1 = self.builder.get_object("window1")
+		self.window1.show()
 
-	def install_app(self, apps, *args):
+	def install_app(self, null, name_app):
 		scripts_path = os.getcwd() + "/apps/"
-		print (scripts_path)
-		os.system("gnome-terminal -x bash -c %s%s && exit; exec bash" %(scripts_path, apps))
-		print (scripts_path, app)
-
-	#def setup(self):
-		#for i in self.applications:
-			#button_name = "%s_btn" % i
-			#getattr(self.main, button_name).connect('clicked', self._on_app_btn__clicked, i)
+		print ("Executando Script", (scripts_path + name_app))
+		os.system("gnome-terminal -x bash -c '%s%s' && exit; exec bash" %(scripts_path, name_app))
 
 if __name__ == '__main__':
     app = App()
