3cdcafeac9706de895707937a451dc1718aa033c
Raphael Sander <raphael.sander75@gmail.com>
2016-09-27 11:35:41 -0300

    Ajustes na aparência.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 import os
 import gi
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
+from gi.repository import Gtk, Gdk
 
 if os.path.exists(os.getcwd() + "/.cacheappinstall/") == True:
     pass
@@ -20,12 +20,27 @@ class App():
 		self.builder.add_from_file(os.getcwd() + "/ui/main.glade")
 		self.builder.connect_signals(self)
 		self.window1 = self.builder.get_object("window1")
+		self.window2 = self.builder.get_object("window2")
 		self.aboutdialog = self.builder.get_object("aboutdialog1")
+
+		self.window1.set_name('window1')
+		self.window2.set_name('window2')
 
 		home = ["apps", "themes", "icons", "fonts", "cursor", "conky", "other"]
 
 		for y in home:
 			self.builder.get_object("%s" %y).connect('clicked', self.aplicativos, "%s" %y)
+
+		style_provider = Gtk.CssProvider()
+
+		css = """
+				@import url("%s");
+		        """ %(os.getcwd() + "/ui/style.css")
+		style_provider.load_from_data(bytes(css.encode()))
+		Gtk.StyleContext.add_provider_for_screen(
+			Gdk.Screen.get_default(), style_provider,
+			Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+		)
 
 		self.window1.show()
 
@@ -43,14 +58,11 @@ class App():
 		print ("Exibindo janela de %s" %categoria)
 		self.window1.hide()
 
-		try:
-			self.window2.show_all()
-		except:
-			self.window2 = self.builder.get_object("window2")
-			for i in range(0, 9):
-				self.builder.get_object("%s" %i).set_label("%s" %apps[i])
-				self.builder.get_object("%s" %i).connect('clicked', self.install_app, categoria,"%s" %apps[i])
-			self.window2.show_all()
+		self.window2 = self.builder.get_object("window2")
+		for i in range(0, 9):
+			self.builder.get_object("%s" %i).set_label("%s" %apps[i])
+			self.builder.get_object("%s" %i).connect('clicked', self.install_app, categoria,"%s" %apps[i])
+		self.window2.show_all()
 
 	def next(self, data=None):
 		print ("Próxima página")
