fc0af9dd2510f5c0e335519b3f3d1228a6bd4dc8
Raphael Sander <raphael.sander75@gmail.com>
2016-10-16 20:45:42 -0200

    Colocando tudo em lista. Alguns erros para corrigir.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 
 import os
 import gi
+
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gdk
 
@@ -13,97 +14,93 @@ if os.path.exists(os.getcwd() + "/.cacheappinstall/") == True:
 else:
     os.mkdir(os.getcwd() + "/.cacheappinstall/")
 
+
 class App():
 
-	def __init__(self):
-		self.builder = Gtk.Builder()
-		self.builder.add_from_file(os.getcwd() + "/ui/main.glade")
-		self.builder.connect_signals(self)
-		self.window1 = self.builder.get_object("window1")
-		self.window2 = self.builder.get_object("window2")
-		self.aboutdialog = self.builder.get_object("aboutdialog1")
+    def __init__(self):
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file(os.getcwd() + "/ui/main.glade")
+        self.builder.connect_signals(self)
+        self.window1 = self.builder.get_object("window1")
+        self.window2 = self.builder.get_object("window2")
+        self.aboutdialog = self.builder.get_object("aboutdialog1")
 
-		self.window1.set_name('window1')
-		self.window2.set_name('window2')
-		self.aboutdialog.set_name('aboutdialog1')
+        self.window1.set_name('window1')
+        self.window2.set_name('window2')
+        self.aboutdialog.set_name('aboutdialog1')
 
-		global page
-		page = 1
+        home = ["apps", "themes", "icons", "fonts", "cursor", "conky", "other"]
 
-		home = ["apps", "themes", "icons", "fonts", "cursor", "conky", "other"]
+        for y in home:
+            self.builder.get_object("%s" % y).connect('clicked', self.aplicativos, '%s' %y)
 
-		for y in home:
-			self.builder.get_object("%s" %y).connect('clicked', self.aplicativos, "%s" %y)
+        style_provider = Gtk.CssProvider()
 
-		style_provider = Gtk.CssProvider()
+        css = "@import url('%s');" % (os.getcwd() + "/ui/style.css")
 
-		css = "@import url('%s');" %(os.getcwd() + "/ui/style.css")
+        style_provider.load_from_data(bytes(css.encode()))
+        Gtk.StyleContext.add_provider_for_screen(Gdk.Screen.get_default(), style_provider,
+                                                 Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
 
-		style_provider.load_from_data(bytes(css.encode()))
-		Gtk.StyleContext.add_provider_for_screen(Gdk.Screen.get_default(), style_provider, Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
+        self.window1.show()
 
-		self.window1.show()
+    def about(self, data=None):
+        print("Exibindo Sobre")
+        self.response = self.aboutdialog.run()
+        self.aboutdialog.hide()
 
-	def about(self, data=None):
-		print ("Exibindo Sobre")
-		self.response = self.aboutdialog.run()
-		self.aboutdialog.hide()
+    def window1_destroy(self, data=None):
+        print("Fechando")
+        Gtk.main_quit()
 
-	def window1_destroy(self, data=None):
-		print ("Fechando")
-		Gtk.main_quit()
+    def aplicativos(self, button, categoria):
+        apps = os.listdir(os.getcwd() + "/%s/" % categoria)
+        print("Exibindo janela de %s" % categoria)
+        self.window1.hide()
+        self.window2 = self.builder.get_object("window2")
 
-	def aplicativos(self, null, categoria):
-		apps = os.listdir(os.getcwd() + "/%s/" %categoria)
-		print ("Exibindo janela de %s" %categoria)
-		self.window1.hide()
+        listbox = Gtk.ListBox()
+        listbox.set_selection_mode(Gtk.SelectionMode.NONE)
 
-		self.btn = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-		self.btt = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+        for i in apps:
+            print(i)
 
-		for i in range(0, 9):
-			global page
-			v = i * page
-			self.window2 = self.builder.get_object("window2")
-			self.btt[i] = self.builder.get_object("%s" %i)
-			try:
-				self.btt[i].set_label("%s" % apps[v])
-				self.btn[i] = self.btt[i].connect('clicked', self.install_app, categoria, "%s" %apps[v])
-				print(self.btn[i], self.btt[i])
-			except:
-				pass
+            row = Gtk.ListBoxRow()
+            hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
+            row.add(hbox)
+            vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+            hbox.pack_start(vbox, True, True, 0)
 
-		home = self.builder.get_object("home")
-		yuuu = home.connect('clicked', self.home, self.btn, self.btt)
-		self.window2.show_all()
+            label = Gtk.Label("%s" %i, xalign=0)
+            vbox.pack_start(label, True, True, 0)
 
-	def next(self, data=None):
-		global page
-		page = page + 1
-		print(page)
-		print ("Página %s" %page)
-		self.window2.show_all()
+            button = Gtk.Button("Execute")
+            hbox.pack_start(button, False, True, 0)
 
-	def back(self, data=None):
-		global page
-		if page != 1:
-			page = page - 1
-		print("Página %s" %page)
+            listbox.add(row)
 
-	def home(self, button, arg, btt):
-		print ("Voltando ao Início")
-		print(arg)
-		print(btt)
-		for i in range(0, 9):
-			self.btt[i].disconnect(arg[i])
-		self.window2.hide()
-		self.window1.show()
+        scroll = self.builder.get_object("scrolledwindow1")
+        scroll.add_with_viewport(listbox)
 
-	def install_app(self, null, categoria, name_app):
-		scripts_path = os.getcwd() + "/%s/" %categoria
-		print ("Executando Script", (scripts_path + name_app))
-		os.system("chmod +x '%s%s'" %(scripts_path, name_app))
-		os.system("gnome-terminal -x bash -c '%s%s' && exit; exec bash" %(scripts_path, name_app))
+        self.window2.show_all()
+
+    def next(self, data=None):
+        pass
+
+    def back(self, data=None):
+        pass
+
+    def home(self, button):
+        print("Voltando ao Início")
+        self.window2.hide()
+        self.window1.show()
+
+    def install_app(self, null, categoria, name_app):
+        scripts_path = os.getcwd() + "/%s/" % categoria
+        print("Executando Script", (scripts_path + name_app))
+        os.system("chmod +x '%s%s'" % (scripts_path, name_app))
+        os.system("gnome-terminal -x bash -c '%s%s' && exit; exec bash" % (scripts_path, name_app))
+
 
 if __name__ == '__main__':
     app = App()
