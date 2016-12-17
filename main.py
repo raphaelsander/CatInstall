b7db7dd1793c32db44b32dbfe0c299ee94f9b103
Raphael Sander <raphael.sander75@gmail.com>
2016-12-17 12:00:11 -0200

    Alteração para abas para cada categoria de software. Implementação de exceptions. Código funcionando e com estilo aplicado.

--- main.py
+++ main.py
@@ -9,83 +9,81 @@ import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gdk
 
-if os.path.exists(os.getcwd() + "/.cacheappinstall/") == True:
-    pass
-else:
-    os.mkdir(os.getcwd() + "/.cacheappinstall/")
-
-
-class App():
+class App:
 
     def __init__(self):
+        #Criação da pasta de cache
+        if os.path.exists(os.getcwd() + "/.cacheappinstall/"):
+            pass
+        else:
+            try:
+                os.mkdir(os.getcwd() + "/.cacheappinstall/")
+            except:
+                print("Erro ao criar a pasta de cache. Verifique as permissões do software.")
+
+        #Construindo toda a estrutura gráfica
         self.builder = Gtk.Builder()
         self.builder.add_from_file(os.getcwd() + "/ui/main.glade")
+        self.window = self.builder.get_object("window")
+        self.about_dialog = self.builder.get_object("about_dialog")
         self.builder.connect_signals(self)
-        self.window_home = self.builder.get_object("window_home")
-        #self.window2 = self.builder.get_object("window2")
-        self.aboutdialog = self.builder.get_object("aboutdialog1")
+        self.categories = ["app", "theme", "icon", "font", "cursor", "conky", "other"]
 
-        self.window_home.set_name('window_home')
-        #self.window2.set_name('window2')
-        self.aboutdialog.set_name('aboutdialog1')
+        cont = 0
 
-        home = ["app", "theme", "icon", "font", "cursor", "conky", "other"]
+        for self.category in self.categories:
+            print("------------%s------------" %self.category)
+            listbox = self.builder.get_object("listbox_%s" %self.category)
+            for i in os.listdir(os.getcwd() + "/scripts/%s/" % self.category):
+                row = Gtk.ListBoxRow()
+                hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=80)
+                row.add(hbox)
+                vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+                hbox.pack_start(vbox, True, True, 0)
+                label = Gtk.Label("%s" %i, xalign=0)
+                vbox.pack_start(label, True, True, 0)
 
-        for category in home:
-            self.builder.get_object("button_%s" %category).connect('clicked', self.aplicativos, category)
+                button = Gtk.Button("Execute")
+                patch = os.getcwd() + "/scripts/%s/%s" %(self.category, i)
+                print(patch)
+                button.connect('clicked', self.install_app, patch)
+                hbox.pack_start(button, False, True, 0)
+                listbox.add(row)
+
+                cont = cont + 1
+
+        #Definindo estilo CSS
+        self.window.set_name('window')
+        self.about_dialog.set_name('window_about')
 
         style_provider = Gtk.CssProvider()
-
         css = "@import url('%s');" % (os.getcwd() + "/ui/style.css")
-
         style_provider.load_from_data(bytes(css.encode()))
         Gtk.StyleContext.add_provider_for_screen(Gdk.Screen.get_default(), style_provider,
                                                  Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION)
 
-        self.window_home.show()
+        print("Total de %s scripts, ajude a aumentar esse número! Acesse http://blackcat8080.xyz/catinstall" %cont)
+        self.window.show_all()
 
-    def about(self, data=None):
-        self.response = self.aboutdialog.run()
-        self.aboutdialog.hide()
+    #Função responsável por exibir a janela sobre
+    def on_button_about_clicked(self, data=None):
+        self.response = self.about_dialog.run()
+        self.about_dialog.hide()
 
-    def window_home_destroy(self, data=None):
+    #Função responsável por matar o programa após fechar a janela
+    def window_destroy(self, data=None):
         Gtk.main_quit()
 
-    def aplicativos(self, null, category):
-        apps = os.listdir(os.getcwd() + "/scripts/%s/" %category)
-        self.window_home.hide()
-
-        self.window2 = self.builder.get_object("window_%s" %category)
-        listbox = self.builder.get_object("listbox_%s" %category)
-
-        for i in apps:
-
-            row = Gtk.ListBoxRow()
-            hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=80)
-            row.add(hbox)
-            vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
-            hbox.pack_start(vbox, True, True, 0)
-
-            label = Gtk.Label("%s" %i, xalign=0)
-            vbox.pack_start(label, True, True, 0)
-
-            button = Gtk.Button("Execute")
-            button.connect('clicked', self.install_app, category, '%s' %i)
-            hbox.pack_start(button, False, True, 0)
-
-            listbox.add(row)
-
-        self.window2.show_all()
-
-    def home(self, button):
-        self.window2.hide()
-        self.window_home.show()
-
-    def install_app(self, null, category, name_app):
-        scripts_path = os.getcwd() + "/scripts/%s/" %category
-        os.system("chmod +x '%s%s'" %(scripts_path, name_app))
-        os.system("gnome-terminal -x bash -c '%s%s' && exit; exec bash" %(scripts_path, name_app))
-
+    #Função responsável por executar os scripts
+    def install_app(self, widget, patch):
+        try:
+            os.system("chmod +x '%s'" % patch)
+        except:
+            print("Erro ao trocar a permissão do script. Verifique as permissões.")
+        try:
+            os.system("gnome-terminal -x bash -c '%s' && exit; exec bash" % patch)
+        except:
+            print("Erro ao executar o script. Verifique as permissões e o código do mesmo.")
 
 if __name__ == '__main__':
     app = App()
