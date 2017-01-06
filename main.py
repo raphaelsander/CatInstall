cb60ee4e58da6b9171de20e405ab5f0cd6dddaf9
Raphael Sander <raphael.sander75@gmail.com>
2017-01-05 23:02:32 -0200

    PEP8

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from gi.repository import Gtk, Gdk
 class App:
 
     def __init__(self):
-        #Criação da pasta de cache
+        # Criação da pasta de cache
         if os.path.exists(os.getcwd() + "/.cacheappinstall/"):
             pass
         else:
@@ -21,7 +21,7 @@ class App:
             except:
                 print("Erro ao criar a pasta de cache. Verifique as permissões do software.")
 
-        #Construindo toda a estrutura gráfica
+        # Construindo toda a estrutura gráfica
         self.builder = Gtk.Builder()
         self.builder.add_from_file(os.getcwd() + "/ui/main.glade")
         self.window = self.builder.get_object("window")
@@ -32,19 +32,19 @@ class App:
         cont = 0
 
         for self.category in self.categories:
-            print("------------%s------------" %self.category)
-            listbox = self.builder.get_object("listbox_%s" %self.category)
+            print("------------%s------------" % self.category)
+            listbox = self.builder.get_object("listbox_%s" % self.category)
             for i in os.listdir(os.getcwd() + "/scripts/%s/" % self.category):
                 row = Gtk.ListBoxRow()
                 hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=80)
                 row.add(hbox)
                 vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
                 hbox.pack_start(vbox, True, True, 0)
-                label = Gtk.Label("%s" %i, xalign=0)
+                label = Gtk.Label("%s" % i, xalign=0)
                 vbox.pack_start(label, True, True, 0)
 
                 button = Gtk.Button("Execute")
-                patch = os.getcwd() + "/scripts/%s/%s" %(self.category, i)
+                patch = os.getcwd() + "/scripts/%s/%s" % (self.category, i)
                 print(patch)
                 button.connect('clicked', self.install_app, patch)
                 hbox.pack_start(button, False, True, 0)
@@ -52,7 +52,7 @@ class App:
 
                 cont = cont + 1
 
-        #Definindo estilo CSS
+        # Definindo estilo CSS
         self.window.set_name('window')
         self.about_dialog.set_name('window_about')
 
@@ -65,16 +65,16 @@ class App:
         print("Total de %s scripts, ajude a aumentar esse número! Acesse http://blackcat8080.xyz/catinstall" %cont)
         self.window.show_all()
 
-    #Função responsável por exibir a janela sobre
+    # Função responsável por exibir a janela sobre
     def on_button_about_clicked(self, data=None):
         self.response = self.about_dialog.run()
         self.about_dialog.hide()
 
-    #Função responsável por matar o programa após fechar a janela
+    # Função responsável por matar o programa após fechar a janela
     def window_destroy(self, data=None):
         Gtk.main_quit()
 
-    #Função responsável por executar os scripts
+    # Função responsável por executar os scripts
     def install_app(self, widget, patch):
         try:
             os.system("chmod +x '%s'" % patch)
