02d18909bd60922cb937c36aaefbbbdad52d37b7
Raphael Sander <raphael.sander75@gmail.com>
2017-02-05 01:47:21 -0200

    Adição de tela de carregamento ao conferir scripts locais com o servidor.

--- main.py
+++ main.py
@@ -4,38 +4,19 @@
 # By Team BlackCat
 
 import os
-import gi
+import sys
+import threading
 import json
 import requests
 import urllib2
 import hashlib
+import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, Gdk
 
-class App:
+class App(threading.Thread):
 
     def __init__(self):
-        # Criando pasta de Cache onde será salvo os downloads dos scripts
-        if os.path.exists(os.getcwd() + "/.cache/"):
-            pass
-        else:
-            try:
-                os.mkdir(os.getcwd() + "/.cache/")
-            except:
-                print("Erro ao criar a pasta de cache. Verifique as permissões do software.")
-
-        # Atualizando CHANGELOG
-        url = urllib2.urlopen("http://catinstall.blackcat8080.xyz/CHANGELOG")
-        os.remove("CHANGELOG")
-        with open('CHANGELOG', 'wb') as output:
-            output.write(url.read())
-
-        # Abrindo arquivo de CHANGELOG que irá efetuar o controle de versão dos scripts
-        try:
-            changelog = open('CHANGELOG', 'r')
-            data_changelog = json.load(changelog)
-        except:
-            print("ERROR 05")
 
         # Construindo as janelas do app via XML Glade
         self.builder = Gtk.Builder()
@@ -44,23 +25,7 @@ class App:
         self.about_dialog = self.builder.get_object("about_dialog")
         self.builder.connect_signals(self)
 
-        # Carregando o arquivo de CHANGELOG e transformando os scripts em objetos
-        for category in data_changelog:
-            for script in data_changelog['%s' % category]:
-                name = data_changelog[category][script]['name']
-                patch = os.getcwd() + data_changelog[category][script]['patch']
-                md5 = data_changelog[category][script]['md5']
-
-                script = Script(name, category, patch, md5)
-
-                script.update(name, category, patch, md5)
-
-                script.add_row(name, category, patch, self.builder)
-
-        # Definindo nome para as janelas para aplicar estilo através do CSS
-        self.window.set_name('window')
-        self.about_dialog.set_name('window_about')
-
+        # Definindo estilo através do CSS
         style_provider = Gtk.CssProvider()
         css = "@import url('%s');" % (os.getcwd() + "/ui/style.css")
         style_provider.load_from_data(bytes(css.encode()))
@@ -69,11 +34,81 @@ class App:
 
         # Exibindo interface gráfica
         self.window.show_all()
+        threading.Thread.__init__(self)
 
+    def run(self):
+
+        # Criando pasta de cache.
+        if os.path.exists(os.path.expanduser("~/.cache/")):
+            pass
+        else:
+            try:
+                os.mkdir(os.path.expanduser("~/.cache/"))
+            except OSError:
+                print("Erro ao criar a pasta de cache. Verifique as permissões do software.")
+
+        # Criando pasta onde serão salvos os scripts e dados do software.
+        if os.path.exists(os.path.expanduser("~/.local/share/CatInstall/")):
+            pass
+        else:
+            try:
+                os.mkdir(os.path.expanduser("~/.local/share/CatInstall/"))
+                os.mkdir(os.path.expanduser("~/.local/share/CatInstall/scripts/"))
+            except OSError:
+                print("Erro ao criar a pasta de dados do software. Verifique as permissões do software. :)")
+
+        # Atualizando CHANGELOG
+        url = urllib2.urlopen("http://catinstall.blackcat8080.xyz/CHANGELOG")
+        try:
+            os.remove(os.path.expanduser("~/.local/share/CatInstall/CHANGELOG"))
+        except OSError:
+            print("Erro ao remover CHANGELOG, talvez o arquivo não exista. :)")
+
+        try:
+            with open(os.path.expanduser("~/.local/share/CatInstall/CHANGELOG"), 'wb') as output:
+                output.write(url.read())
+        except OSError:
+            print("Erro ao criar arquivo CHANGELOG, verifique as permissões do software. :)")
+
+        # Abrindo arquivo de CHANGELOG que irá efetuar o controle de versão dos scripts
+        try:
+            changelog = open(os.path.expanduser("~/.local/share/CatInstall/CHANGELOG"), 'r')
+            data_changelog = json.load(changelog)
+        except IOError:
+            print("Arquivo de CHANGELOG não existe. É necessário internet!!!! :)")
+
+        for category in data_changelog:
+
+            # Criando pasta para cada categoria de script
+            if os.path.exists(os.path.expanduser("~/.local/share/CatInstall/scripts/%s" % category)):
+                pass
+            else:
+                try:
+                    os.mkdir(os.path.expanduser("~/.local/share/CatInstall/scripts/%s" % category))
+                except OSError:
+                    print("Erro ao criar a pasta de script para %s. Verifique as permissões do software. :)" % category)
+
+            # Carregando o arquivo de CHANGELOG e transformando os scripts em objetos
+            for script in data_changelog['%s' % category]:
+                name = data_changelog[category][script]['name']
+                patch = os.path.expanduser("~/.local/share/CatInstall") + data_changelog[category][script]['patch']
+                md5 = data_changelog[category][script]['md5']
+
+                script = Script(name, category, patch, md5)
+
+                script.update(name, category, patch, md5)
+
+                script.add_row(name, category, patch, self.builder)
+
+        self.builder.get_object("spinner").hide()
+        self.builder.get_object("notebook").show()
+
+    # Função responsável pela janela Sobre
     def on_button_about_clicked(self, data=None):
         self.response = self.about_dialog.run()
         self.about_dialog.hide()
 
+    # Função responsável por matar o Gtk após fechar o aplicativo
     def window_destroy(self, data=None):
         Gtk.main_quit()
 
@@ -92,6 +127,7 @@ class Script(object):
         row = Gtk.ListBoxRow()
         hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=80)
         row.add(hbox)
+        row.show()
         vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
         hbox.pack_start(vbox, True, True, 0)
         label = Gtk.Label("%s" % name, xalign=0)
@@ -102,11 +138,17 @@ class Script(object):
         hbox.pack_start(button, False, True, 0)
         listbox.add(row)
 
+        vbox.show()
+        hbox.show()
+        row.show()
+        button.show()
+        label.show()
+
     def update(self, name, category, patch, md5):
 
         hash_md5 = hashlib.md5()
 
-        # Verificando se o script existe localmente, caso não exista baixa diretamento do site
+        # Verificando se o script existe localmente, caso não exista baixa diretamente do site
         if os.path.isfile(patch):
             pass
         else:
@@ -115,7 +157,7 @@ class Script(object):
                 with open('%s' %patch, 'wb') as output:
                     output.write(url.read())
             except IOError:
-                print("Error 04")
+                print("Não foi possível baixar e escrever o script. Reinicie o aplicativo com internet!!! :)")
 
         # Extraindo MD5 do script local
         try:
@@ -123,30 +165,34 @@ class Script(object):
                 for ped in iter(lambda: file.read(4096), b""):
                     hash_md5.update(ped)
         except IOError:
-            print("Error 03")
+            print("Não foi possível ler o script local. Reinicie o aplicativo com internet!!! :)")
 
         # Comparando o MD5 do CHANGELOG e do script local, caso diferente é feito o download novamente
         if hash_md5.hexdigest() == md5:
             pass
         else:
             try:
+                os.remove("%s" % patch)
+            except OSError:
+                print("Não foi possível excluir o script desatualizado. Talvez ele tenha sumido. :/")
+            try:
                 url = urllib2.urlopen("http://catinstall.blackcat8080.xyz/scripts/%s/%s" % (category, patch.split("/")[-1]))
-                os.remove("%s" %patch)
                 with open('%s' %patch, 'wb') as output:
                     output.write(url.read())
             except IOError:
-                print("Error 00")
+                print("Não foi possível baixar e escrever o script. Reinicie o aplicativo com internet!!! :)")
 
     def install(self, button, patch):
         try:
             os.system("chmod +x '%s'" %patch)
-        except IOError:
-            print("Error 01")
+        except OSError:
+            print("Não foi possível alterar as permissões do script. :/")
         try:
             os.system("gnome-terminal -x bash -c '%s' && exit; exec bash" %patch)
-        except IOError:
-            print("Error 02")
+        except OSError:
+            print("Não foi possível executar o script. Será que você possui o gnome-terminal? :/")
 
 if __name__ == '__main__':
     app = App()
+    app.start()
     Gtk.main()
